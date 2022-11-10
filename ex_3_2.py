84dfa4ec59e4602f525edaf95cddc640ad073322
leonkuehner <108261905+leonkuehner@users.noreply.github.com>
2022-11-10 16:50:17 +0100

    Add files via upload

---
+++ ex_3_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import scipy.linalg
+
+
+def lj_potential(r_ij: np.ndarray) -> float:
+
+    pass
+
+def lj_force(r_ij: np.ndarray) -> np.ndarray:
+
+    pass
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    pass
